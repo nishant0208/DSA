c2150279f599a0a1b1dfc3addd267f3502729348
Nishant Tiwary <79209963+nishant0208@users.noreply.github.com>
2025-07-24 01:01:03 +0530

    Time: 4 ms (10.55%) | Memory: 44.6 MB (58.38%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,12 +1,15 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        String result = "";
-        for (int i =0;i<s.length();i++)
-        {
-            for(int j = 0; j<s.length(); j++)
-            if(indices[j] == i)
-            result = result + s.charAt(j);
+        StringBuilder ans=new StringBuilder();
+        // Char ansarray[]=new Char[indices.length];
+        for( int i=0;i<indices.length;i++){
+         for(int j=0;j<indices.length;j++){
+            if(i==indices[j]){
+                ans.append(s.charAt(j));
+                // ansarray[i]==s.charAt(j);
+            }
+         }
         }
-        return result;
+        return ans.toString();
     }
 }
