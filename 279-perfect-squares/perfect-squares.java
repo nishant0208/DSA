e698ac116b5e8773b404190cab2c9808d160af21
Nishant Tiwary <79209963+nishant0208@users.noreply.github.com>
2025-09-17 00:51:53 +0530

    Time: 29 ms (78.49%) | Memory: 42.5 MB (89.14%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSquares(int n) {
+         int[] dp = new int[n + 1];
+        
+        // base case
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int x = 1; x * x <= i; ++x) {
+              
+                // recursive case
+                dp[i] = Math.min(dp[i], 1 + dp[i - x * x]);
+            }
+        }
+        
+        return dp[n];
+    }
+}
