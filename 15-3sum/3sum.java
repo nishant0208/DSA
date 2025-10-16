372c1bf7e51426c22954f901d82d108686d88d86
Nishant Tiwary <79209963+nishant0208@users.noreply.github.com>
2025-10-17 04:20:14 +0530

    Time: 838 ms (12.06%) | Memory: 52 MB (29.51%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,21 +1,34 @@
-public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums.length < 3) return result;
-        Arrays.sort(nums);
-        int i = 0;
-        while(i < nums.length - 2) {
-            if(nums[i] > 0) break;
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0) result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                if(sum <= 0) while(nums[j] == nums[++j] && j < k);
-                if(sum >= 0) while(nums[k--] == nums[k] && j < k);
-            }
-            while(nums[i] == nums[++i] && i < nums.length - 2);
-        }
-        return result;
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        
+ 
+
+    if (arr == null || arr.length < 3) return new ArrayList<>();
+
+
+    Arrays.sort(arr);
+    Set<List<Integer>> result = new HashSet<>();
+
+  
+    for (int i = 0; i < arr.length - 2; i++)
+    {
+      int left = i + 1;
+      int right = arr.length - 1;
+
+      while (left < right) {
+        int sum = arr[i] + arr[left] + arr[right];
+
+        if (sum == 0) {
+          result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+          left++;
+          right--;
+        } else if (sum < 0)
+          left++;
+        else
+          right--;
+      }
     }
+    return new ArrayList<>(result);
+  }
+
 }
