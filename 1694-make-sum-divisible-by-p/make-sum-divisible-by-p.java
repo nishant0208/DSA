e5dfe96e4577ad41c06c7bd0751824e58e4e86a2
Nishant Tiwary <79209963+nishant0208@users.noreply.github.com>
2025-12-01 02:47:55 +0530

    Time: 28 ms (56.60%) | Memory: 98.2 MB (49.79%) - LeetSync

---
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum = (sum + num) % p;
+        }
+
+        int target = sum % p;
+        if (target == 0) return 0;
+
+        Map<Integer , Integer> mp = new HashMap<>();
+        mp.put(0 , -1);
+
+        int res = n;
+        int curr = 0;
+
+        for (int j = 0; j < n; j++) {
+            curr = (curr + nums[j]) % p;
+
+            int rem = (curr - target + p) % p;
+            if (mp.containsKey(rem)) res = Math.min(res , j - mp.get(rem));
+
+            mp.put(curr , j);
+        }
+        return res == n ? -1 : res;
+    }
+}
